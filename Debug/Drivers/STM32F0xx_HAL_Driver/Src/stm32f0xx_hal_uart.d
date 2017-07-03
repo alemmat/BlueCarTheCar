6a0df47083f471a413db0322b8597f3f0056b435
’jorge <alemmat@gmail.com>
2017-07-02 23:45:47 -0300

    First commit

---
+++ Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.d
@@ -0,0 +1,89 @@
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ /home/jorge/workspace/bluecar/Inc/stm32f0xx_hal_conf.h \
+ /home/jorge/workspace/bluecar/Inc/main.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cm0.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ /home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+/home/jorge/workspace/bluecar/Inc/stm32f0xx_hal_conf.h:
+
+/home/jorge/workspace/bluecar/Inc/main.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cm0.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/jorge/workspace/bluecar/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+/home/jorge/workspace/bluecar/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
